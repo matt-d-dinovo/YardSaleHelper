4ae0642da58c21645b393f775fb484d146c98b87
Matt D DiNovo <matt.d.dinovo@icloud.com>
2025-12-03 15:23:50 -0600

    Initial Commit

---
+++ YardSaleHelper/YardSaleHelperApp.swift
@@ -0,0 +1,17 @@
+//
+//  YardSaleHelperApp.swift
+//  YardSaleHelper
+//
+//  Created by Matthew DiNovo on 12/3/25.
+//
+
+import SwiftUI
+
+@main
+struct YardSaleHelperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
